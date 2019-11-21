7b22dbbc1ceb5980a0ee3938073787bec26e01ff
tenel <ten.elena@outlook.com>
2019-11-21 18:17:55 +0100

    Update of tests with alternative solutions

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,37 +1,148 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Calculator {
 
-    private int number;
+    //private int number;
+    private String input;
     private int anotherNumber;
 
     public Calculator() {
     }
 
-    public Calculator(int number) {
-        this.number = number;
+    public Calculator(String input) {
+        this.input = input;
     }
 
-    public Calculator(int number, int anotherNumber) {
-        this.number = number;
-        this.anotherNumber = anotherNumber;
+    public String getInput() {
+        return input;
     }
 
-    public static int division(int a, int b) {
-        return a / b;
+    public double convertStringToDouble(String input){
+        double number = 0.0;
+        try {
+            number =Double.parseDouble(input);
+        } catch (NumberFormatException e) {
+            inputError();
+        }
+        return number;
     }
 
-    public static int multiply(int a, int b) {
-        return a * b;
+    private void inputError() {
+        System.out.println("Wrong format. Should be numbers!");
+        throw new NumberFormatException();
     }
 
-    public static int add(int a, int b) {
-        return a + b;
+    private void division() {
+        int a=  (int) convertStringToDouble(input);
+        int b=  (int)convertStringToDouble(input);
+        divide(a,b);
+    }
+
+    private void divisionError() {
+        System.out.println("Error! Impossible to divide by 0!");
+        throw new ArithmeticException();
+    }
+
+    private void multiply() {
+        double number1=  convertStringToDouble(input);
+        double number2=  convertStringToDouble(input);
+
+        multiplication(number1,number2);
+    }
+
+    private void add() {
+        double number1=  convertStringToDouble(input);
+        double number2=  convertStringToDouble(input);
+        addMethod(number1, number2);
+    }
+
+    public double addMethod(double a, double b){
+        return a+b;
+    }
+
+    private void substraction() {
+        int a=  (int) convertStringToDouble(input);
+        int b=  (int)convertStringToDouble(input);
+        substract(a,b);
     }
 
     public static boolean isSqrtPositive(int number) {
         return Math.sqrt(number) >= 0;
     }
 
+    public double multiplication(double i, double i1) {
+        return i*i1;
+    }
+
+    public double substract(int i, int i1) {
+        return (double)i-i1;
+    }
+
+    public double divide(int i, int i1) {
+        if (i1 == 0) {
+            divisionError();
+        }
+
+        return (double)i/i1;
+    }
+
+    public List<String> menu(){
+        List<String> list = new ArrayList<>();
+        list.add("1. +");
+        list.add("2. -");
+        list.add("3. x");
+        list.add("4. /");
+        list.add("5. Exit");
+
+        return list;
+    }
+    private int convertStringToInt(String str){
+        int number = 0;
+        try {
+            number = Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            convertError();
+        }
+        return number;
+    }
+
+    public void choices(String str) {
+        int choice = convertStringToInt(str);
+        List<String> list = menu();
+        for (String s : list) {
+            System.out.println(s);
+        }
+        switch (choice) {
+            case 1:
+                add();
+            case 2:
+                substraction();
+            case 3:
+                multiply();
+            case 4:
+                division();
+            case 5:
+                return;
+            default:
+                errorChoice();
+
+        }
+    }
+        private void errorChoice() {
+            System.out.println("Please number 1 to 5.");
+            throw new NumberFormatException();
+        }
+        private void convertError(){
+            System.out.println("It must be a number");
+            throw new NumberFormatException();
+        }
+
+    /*
+    public Calculator(int number, int anotherNumber) {
+        this.number = number;
+        this.anotherNumber = anotherNumber;
+    }
     public int getNumber() {
         return number;
     }
@@ -45,13 +156,16 @@ public class Calculator {
         return array.length;
     }
 
-    public static double areaOfRightAngleTriangleA(double c){
-        c = areaOfRectangleFromTriangelA(6.0,1.0)/2;
+    public double areaOfRightAngleTriangleA(double a, double b){
+        double c = areaOfRectangleFromTriangelA(a , b)/2;
         return c;
     }
 
     private static double areaOfRectangleFromTriangelA(double a, double b){
         double c = a*b;
         return c;
+    }*/
+
+
+
     }
-}
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -2,12 +2,109 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class CalculatorTest {
+    private Calculator calculator;
+
+    @BeforeEach
+    void initEach(){
+        calculator = new Calculator();
+        System.out.println("New Calculator is set up.");
+    }
+    @AfterEach
+    void afterEach(){
+        calculator = null;
+        System.out.println(" clear up ");
+    }
 
     @Test
+    @DisplayName("Test of a default constructor")
+    void constructorTest(){
+        Calculator calculat = new Calculator("7");
+        Assertions.assertEquals("7",calculat.getInput() );
+    }
+
+    @Test
+    void additionTest () {
+        Assertions.assertEquals(5.0, calculator.addMethod(3, 2));
+    }
+    @Test
+    void convertStringToDoubleTest(){
+        Assertions.assertEquals(9.0, calculator.convertStringToDouble("9"));
+    }
+    @Test
+    void convertStringToDoubleExceptionTest(){
+        Assertions.assertThrows(NumberFormatException.class, () ->
+                calculator.convertStringToDouble("p"));
+    }
+    @Test
+    @Disabled
+    @DisplayName("Multiply Test is disabled on purpose to check how it works")
+    void multiplyTest(){
+        Assertions.assertEquals(6.0,calculator.multiplication(9,3));
+    }
+    @Test
+    void substractionTest(){
+        Assertions.assertEquals(6, calculator.substract(-10,-16));
+    }
+
+    @Nested
+    @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+    class DivisionTest{
+
+        @Test
+        @Order(1)
+        void divisionTest(){
+            Assertions.assertEquals(6,calculator.divide(6,1));
+        }
+       @Test
+       @Order(2)
+       void divisionExceptionTest(){
+           Assertions.assertThrows(ArithmeticException.class, () ->
+                   calculator.divide(6,0));
+       }
+    }
+
+    @ParameterizedTest
+    @ValueSource (ints = {5})
+    void sizeTest(int number){
+        List<String> list = calculator.menu();
+        Assertions.assertEquals(number, list.size());
+    }
+
+    @DisplayName("Test of a private method via virtual method using 'invoke'")
+    @Test
+    void convertStringToIntTest() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method method = Calculator.class.getDeclaredMethod("convertStringToInt", String.class);
+        method.setAccessible(true);
+        int number = (int) method.invoke(calculator, "3");
+        Assertions.assertEquals(3, number);
+    }
+
+    @Test
+    void errorChoiceTest(){
+        Assertions.assertThrows(NumberFormatException.class, ()->
+                calculator.choices("99"));
+    }
+
+    @Test
+    @Disabled
+    void convertErrortest() throws NoSuchMethodException {
+        Method method = Calculator.class.getDeclaredMethod("convertStringToInt", String.class);
+        method.setAccessible(true);
+        Assertions.assertThrows(NumberFormatException.class, ()->
+                method.invoke(calculator, "p"));
+    }
+
+
+    /*@Test
     @DisplayName("Test of triangel area")
     public void triangelAreaTest(){
-       Assertions.assertEquals(4.0,4.0);
+       Assertions.assertEquals(4.0,calculator.areaOfRightAngleTriangleA(4.0));
     }
 
     @Test
@@ -60,12 +157,12 @@ public class CalculatorTest {
     @BeforeEach
     @Order(1)
     public void initEach(){
-    Calculator calculator = new Calculator();
+    calculator = new Calculator();
     System.out.println("New Calculator is set up.");
     }
 
     //@AfterEach
     //public void cleanUpEach(){
      //   System.out.println("Skickar printraden ändå....även om jag inte skulle");
-    //}
+    //}*/
 }
